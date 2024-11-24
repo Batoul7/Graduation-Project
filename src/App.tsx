8c43060cd9e8c8ed891dc5a790266e5355fc0e65
Batoul7 <batoulab268@gmail.com>
2024-11-24 19:17:30 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-myprimary text-white text-center p-5 font-bold text-3xl'>Welcome to My Super Team</div>
+    </>
+  )
+}
+
+export default App
