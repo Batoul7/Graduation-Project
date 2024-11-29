f8efece8486a4d4faf4265b3ef32090c823626f0
Kinda Ghanem <ghanemkinda0@gmail.com>
2024-11-29 19:14:56 +0300

    Add Hero Home And Hero Common and Button Common

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,15 +3,54 @@
 
 // import Testimonials from "../components/Testimonials/Testimonials";
 
+import HeroHome from "../components/HeroHome";
 
+import imgpeople from './../assets/images/HomePage/SubContainer-1.png'
+import imgArrowHero from './../assets/images/Arrows/goArrow.png'
+
+import imgEyesBtn from './../assets/images/common/eye-yellow.png'
+
+interface HeroCard {
+  id:number;
+  number:string ;
+  mult:string;
+  parcard:string;
+}
 
 
 export default function HomePage() {
 
+
+  
+  const HeroCard: Array<HeroCard> = [
+    {
+      id: 1,
+      number: '300',
+      mult: "+",
+      parcard: "Resources available"
+    } ,
+    {
+      id: 2,
+      number: '12k',
+      mult: "+",
+      parcard: "Total Downloads"
+    } ,
+    {
+      id: 3,
+      number: '10k',
+      mult: "+",
+      parcard: "Active Users"
+    } ,
+
+  ]
+
+
   return (
     <div>
-      home
       {/* <Testimonials/> */}
+
+      <HeroHome HeroCard={HeroCard} par1={"Your Journey to Tomorrow Begins Here"} title={"Explore the Frontiers of Artificial Intelligence"} par2={"Welcome to the epicenter of AI innovation. FutureTech AI News is your passport to a world where machines think, learn, and reshape the future. Join us on this visionary expedition into the heart of AI."} img1={imgpeople} altimg1={"people image"} subtitle={"Explore 1000+ resources"} par3={"Over 1,000 articles on emerging tech trends and breakthroughs."} contentBtn={"Explore Resources"} imgArrowbtn={imgArrowHero} altimagebtn={"image arrow btn"} imgEyes={imgEyesBtn} />
+
     </div>
   );
 };
