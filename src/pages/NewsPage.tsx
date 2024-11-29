48869316fb9fdd8d43f9602aaf3f26766dfb8cde
Safa Hadid <safahadid3@gmail.com>
2024-11-29 22:08:32 +0200

    Latest Blogs Component

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,6 +1,8 @@
 // import { useSelector} from 'react-redux';
 // import { RootState } from "../redux/store";
 
+import LatestBlogs from "../components/LatestBlogs/LatestBlogs";
+
 export default function NewsPage() {
 
   // const blogs = useSelector((state: RootState) => state.newsBlog.blogs)
@@ -8,6 +10,7 @@ export default function NewsPage() {
   return (
     <div>
       News
+      <LatestBlogs />
     </div>
   )
 }
