866d59e1c86c21dc4a829592acb1432b17f591b4
Kinda Ghanem <ghanemkinda0@gmail.com>
2024-11-30 23:46:02 +0300

    update hero

--- src/components/HeroHome/HeroHome.tsx
+++ src/components/HeroHome/HeroHome.tsx
@@ -27,28 +27,28 @@ interface HeroProps {
     imgEyes:string;
 }
 
-// pt-41
+// pt-4[170px]
 
 
 export default function HeroHome({ HeroCard , par1 , title , par2 , img1 , altimg1 , subtitle , par3 , contentBtn , imgArrowbtn, altimagebtn ,imgEyes } :HeroProps ) {
 
   return (
     <div className="px-mainpaddingsmall xl:px-mainpaddingmedium 2xl:px-mainpaddinglarge bg-myprimary-dark-08 flex flex-wrap  sm:h-screen ">
-        <div className="flex flex-col justify-between sm:w-2.5/4 pt-10 sm:pt-25 2xl:pt-37 ">
+        <div className="flex flex-col justify-between sm:w-[59.09%] pt-10 sm:pt-[100px] 2xl:pt-[150px] ">
             <div className="flex flex-col justify-center items-start border-neutral-800 " >
-                <p className="font-medium font-kumbhSans text-mysecondary-dark-40  text-lg xl:text-1.5xl 2xl:text-3xl leading-6.7 xl:leading-6.8 2xl:leading-9 pb-4 xl:pb-5 2xl:pb-7.5 ">{par1}</p>
+                <p className="font-medium font-kumbhSans text-mysecondary-dark-40  text-lg xl:text-1.5xl 2xl:text-3xl leading-[21.6px] xl:leading-[26.4px] 2xl:leading-9 pb-4 xl:pb-5 2xl:pb-7.5 ">{par1}</p>
                 <HeroTitleH1 title={title} mainhero='mainhero' newcommonhero={""} podcastcommonhero={""} Resourcecommonhero={""} />
-                <p className="font-normal text-myprimary-gray-50 text-sm xl:text-base 2xl:text-lg leading-6.7 xl:leading-6 2xl:leading-7 pb-7.5 sm:pb-0 ">{par2}</p>
+                <p className="font-normal text-myprimary-gray-50 text-sm xl:text-base 2xl:text-lg leading-[21.6px] xl:leading-6 2xl:leading-7 pb-7.5 sm:pb-0 ">{par2}</p>
             </div>
-            <div className="flex pb-2.5  border-neutral-800 border border-l-0 border-r-0 border-b-0 sm:[&>*:nth-child(2)]:w-82 [&>*:nth-child(1)]:border-l-0 [&>*:nth-child(1)]:ps-0 [&>*:nth-child(3)]:border-e-0  ">
+            <div className="flex pb-2.5  border-neutral-800 border border-l-0 border-r-0 border-b-0 sm:[&>*:nth-child(2)]:w-[347px] [&>*:nth-child(1)]:border-l-0 [&>*:nth-child(1)]:ps-0 [&>*:nth-child(3)]:border-e-0  ">
                 <CardNumberCounter HeroCard={HeroCard} />
             </div>
         </div>
-        <div className="bg-[url('./assets/images/HomePage/HeroDesign.png')] bg-cover bg-no-repeat bg-center ps-10 xl:ps-20 flex flex-col justify-end sm:w-4.5/12 pb-10 xl:pb-13 2xl:pb-20 border-neutral-800 border-l ">
-            <img src={img1} alt={altimg1} className=" w-43 2xl:w-53 pb-5 2xl:pb-7.5" ></img>
-            <h5 className="font-medium text-lg xl:text-xl 2xl:text-2xl text-white pb-0.5 xl:pb-1 2xl:pb-3.5 leading-6.5 xl:leading-7.5 2xl:leading-9 ">{subtitle}</h5>
-            <p className="font-medium text-sm xl:text-base 2xl:text-lg  text-myprimary-gray-60 pb-5 2xl:pb-7.5 leading-5 xl:leading-5.5 2xl:leading-6.5 ">{par3}</p>
-            <ButtonCommon herobtnAndCommon={'herobtn'} navbtn={''} contentBtn={contentBtn} imgArrowbtn={imgArrowbtn} altimagebtn={altimagebtn} mainsmall={"mainsmall"} filterBtn={""} hiddenArrow={""} EbookHome={""} imgEyes={imgEyes} hiddenEyes={"hiddenEyes"} footerBtn={""}  />
+        <div className="bg-[url('./assets/images/HomePage/HeroDesign.png')] bg-cover bg-no-repeat bg-center ps-10 xl:ps-20 flex flex-col justify-end sm:w-[40.91%] pb-10 xl:pb-[50px] 2xl:pb-20 border-neutral-800 border-l ">
+            <img src={img1} alt={altimg1} className=" w-[174px] 2xl:w-[218px] pb-5 2xl:pb-7.5" ></img>
+            <h5 className="font-medium text-lg xl:text-xl 2xl:text-2xl text-white pb-0.5 xl:pb-1 2xl:pb-3.5 leading-[27px] xl:leading-[30px] 2xl:leading-9 ">{subtitle}</h5>
+            <p className="font-medium text-sm xl:text-base 2xl:text-lg  text-myprimary-gray-60 pb-5 2xl:pb-7.5 leading-5 xl:leading-[24px] 2xl:leading-[27px] ">{par3}</p>
+            <ButtonCommon herobtnAndCommon={'herobtn'} contentBtn={contentBtn} imgArrowbtn={imgArrowbtn} altimagebtn={altimagebtn} mainsmall={"mainsmall"} imgEyes={imgEyes} hiddenEyes={"hiddenEyes"}  />
         </div>
     </div>
   )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -43,7 +43,11 @@ export default function HomePage() {
 
   return (
     <div>
-    <HeroHome HeroCard={HeroCard} par1={"Your Journey to Tomorrow Begins Here"} title={"Explore the Frontiers of Artificial Intelligence"} par2={"Welcome to the epicenter of AI innovation. FutureTech AI News is your passport to a world where machines think, learn, and reshape the future. Join us on this visionary expedition into the heart of AI."} img1={imgpeople} altimg1={"people image"} subtitle={"Explore 1000+ resources"} par3={"Over 1,000 articles on emerging tech trends and breakthroughs."} contentBtn={"Explore Resources"} imgArrowbtn={imgArrowHero} altimagebtn={"image arrow btn"} imgEyes={imgEyesBtn} />
+    <HeroHome HeroCard={HeroCard} par1={"Your Journey to Tomorrow Begins Here"} 
+    title={"Explore the Frontiers of Artificial Intelligence"} par2={"Welcome to the epicenter of AI innovation. FutureTech AI News is your passport to a world where machines think, learn, and reshape the future. Join us on this visionary expedition into the heart of AI."} 
+    img1={imgpeople} altimg1={"people image"} subtitle={"Explore 1000+ resources"} 
+    par3={"Over 1,000 articles on emerging tech trends and breakthroughs."}
+    contentBtn={"Explore Resources"} imgArrowbtn={imgArrowHero} altimagebtn={"image arrow btn"} imgEyes={imgEyesBtn} />
      <MainTitle title="FutureTech Features" subtitle="Unlock the Power of"/>
       <Testimonials/>
     </div>
