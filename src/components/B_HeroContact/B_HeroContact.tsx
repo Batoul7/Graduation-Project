7876c23077d6c04ad3cf70849272f58448aa9ab0
Batoul7 <batoulab268@gmail.com>
2025-01-11 13:57:30 +0300

    update heroContact

--- src/components/B_HeroContact/B_HeroContact.tsx
+++ src/components/B_HeroContact/B_HeroContact.tsx
@@ -32,19 +32,19 @@ export default function B_HeroContact() {
   ];
 
   return (
-    <div className='h-auto 2xl:h-2xl-hero sm:h-md-hero 2xl:mt-[107px] mt-[77px] px-4 xl:px-20 2xl:px-mainpaddinglarge bg-myprimary-dark-08 text-white h- items-center
+    <div className='h-auto 2xl:h-2xl-hero md:h-md-hero 2xl:mt-[107px] mt-[77px] px-4 xl:px-20 2xl:px-mainpaddinglarge bg-myprimary-dark-08 text-white h- items-center
             grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-x-5 lg:gap-x-7.5 2xl:gap-[50px] xl:flex-nowrap divide-y md:divide-y-0 divide-neutral-800 border-t border-neutral-800'>
       {data.map((item, index) => (
           <div key={index}  
           data-aos="zoom-in-right"
           className='md:flex-1 w-full lg:max-w-[275px] 2xl:max-w-[374px] py-5 md:py-0 '>
-            <h3 className='mb-5 lg:mb-7.5 2xl:mb-[50px] text-base lg:text-lg 2xl:text-xl font-medium'>{item.title}</h3>
+            <h3 className='mb-5 lg:mb-7.5 2xl:mb-[50px] text-base lg:text-lg 2xl:text-xl font-medium'>{item.title}</h3>         
             {item.address && (
-              <p className='text-base 2xl:text-lg text-myprimary-gray-60 mb-3 2xl:mb-[15px] max-w-[308px] mx-auto md:max-w-full'>
+              <p className='text-base 2xl:text-lg text-myprimary-gray-60 mb-3 2xl:mb-[15px] max-w-[308px] md:max-w-full'>
                 {item.address}
               </p>
             )}
-            <div className={`w-fit mx-auto sm:max-w-full md:mx-0
+            <div className={` 
             ${index === data.length -1
                   ? "flex flex-row flex-wrap sm:flex-nowrap gap-2.5 2xl:gap-5 pb-5 " 
                   : "flex flex-col gap-2.5 2xl:gap-[15px] justify-center"
@@ -74,7 +74,8 @@ export default function B_HeroContact() {
               }
             })}
             </div>
-          </div>
+            </div>
+       
         ))}
     </div>
   );
